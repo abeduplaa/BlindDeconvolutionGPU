9730a47784f9786af3e46a91ed9c4a703e284e6f
abeduplaa <abrahamduplaa@gmail.com>
2018-10-02 11:53:40 +0200

    fixed function type issue

--- src/downConvolution.cu
+++ src/downConvolution.cu
@@ -100,7 +100,7 @@ void computeDownConvolutionCPU(float *imgOut, const float *imgIn, const float *k
 	}
 }
 
-void computeDownConvolutionGlobalMemCuda(float *imgOut, const float *imgIn, const float *kernel, int w, int h, int nc, int m, int n)
+void computeDownConvolutionGlobalMemCuda(float *imgOut, const float *imgIn, const float *kernel, const int w, const int h, const int nc, const int m, const int n)
 {
 
 	// allocate block and grid size
--- src/downConvolution.cuh
+++ src/downConvolution.cuh
@@ -4,6 +4,6 @@
 
 void computeDownConvolutionCPU(float *imgOut, const float *imgIn, const float *kernel, int w, int h, int nc, int m, int n);
 
-computeDownConvolutionGlobalMemCuda(float *imgOut, const float *imgIn, const float *kernel, int w, int h, int nc, int m, int n);
+void computeDownConvolutionGlobalMemCuda(float *imgOut, const float *imgIn, const float *kernel, const int w, const int h, const int nc, const int m, const int n);
 
 #endif
